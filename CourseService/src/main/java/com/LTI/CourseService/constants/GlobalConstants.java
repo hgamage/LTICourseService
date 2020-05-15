108c30f327a6bd561c96a11a7ebea8c3c3accd55
hgamage <hgamage@wiley.com>
2020-05-15 10:58:28 +0530

    code refactor

---
+++ CourseService/src/main/java/com/LTI/CourseService/constants/GlobalConstants.java
@@ -0,0 +1,59 @@
+package com.LTI.CourseService.constants;
+
+public final class GlobalConstants {
+    public static String ALL_COURSES = "[\n" +
+            "  " +
+            "{\n" +
+            "    \"wileyCourseId\": 1,\n" +
+            "    \"title\": \"testTitle1\",\n" +
+            "    \"type\": \"testType\",\n" +
+            "    \"createDate\": \"13/05/20 12:25:32\",\n" +
+            "    \"institutionId\": \"testInst\",\n" +
+            "    \"courseStartDate\": \"13/05/20 12:25:32\",\n" +
+            "    \"courseEndDate\": \"13/05/20 12:25:32\"\n" +
+            "  " +
+            "},\n" +
+            "  " +
+            "{\n" +
+            "  " +
+            "\"wileyCourseId\": 2,\n" +
+            "  " +
+            "\"title\": \"testTitle2\",\n" +
+            "  " +
+            "\"type\": \"testType2\",\n" +
+            "  " +
+            "\"createDate\": \"13/05/20 12:25:32\",\n" +
+            "  " +
+            "\"institutionId\": \"testInst2\",\n" +
+            "  " +
+            "\"courseStartDate\": \"13/05/20 12:25:32\",\n" +
+            "  " +
+            "\"courseEndDate\": \"13/05/20 12:25:32\"\n" +
+            "  " +
+            "}\n" +
+            "]";
+    public static String COURSE_OBJECT = "{\n" +
+            "  \"title\": \"TestCourse\",\n" +
+            "  \"type\": \"testing\",\n" +
+            "  \"courseStartDate\": \"13/05/20 12:25:32\",\n" +
+            "  \"courseEndDate\": \"13/05/20 12:25:32\",\n" +
+            "  \"institutionId\": \"13/05/20 12:25:32\"\n" +
+            "}";
+
+    public static String ENROLLMENT_OBJECT = "{\n" +
+            "  \"wileyUserId\": 2,\n" +
+            "  \"userName\": \"testUser\"\n" +
+            "}";
+
+    public static String UPDATE_COURSE_OBJECT = "{\n" +
+            "  \"wileyCourseId\": 55,\n" +
+            "  \"title\": \"Testing\",\n" +
+            "  \"type\": \"test\",\n" +
+            "  \"createDate\": \"14/05/20 16:28:48\",\n" +
+            "  \"institutionId\": \"inst2\",\n" +
+            "  \"courseStartDate\": \"13/05/20 12:25:32\",\n" +
+            "  \"courseEndDate\": \"13/05/20 12:25:32\"\n" +
+            "}";
+
+
+}
--- CourseService/src/test/java/com/LTI/CourseService/resource/CourseResourceTest.java
+++ CourseService/src/test/java/com/LTI/CourseService/resource/CourseResourceTest.java
@@ -1,6 +1,7 @@
 package com.LTI.CourseService.resource;
 
 
+import com.LTI.CourseService.constants.GlobalConstants;
 import com.LTI.CourseService.model.Course;
 import com.LTI.CourseService.model.Response;
 import com.LTI.CourseService.service.CourseService;
@@ -18,14 +19,22 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 public class CourseResourceTest {
 
     private MockMvc mockMvc;
+
+    private GlobalConstants globalConstants;
 
     @InjectMocks
     private CourseResource courseResource;
@@ -43,23 +52,22 @@ public class CourseResourceTest {
 
     @Test
     public void testGetAllTestCasesSuccess() throws Exception {
+
+        when(courseService.getAllCourses()).thenReturn(Stream.of(
+                new Course(1,"testTitle1", "testType","13/05/20 12:25:32","testInst", "13/05/20 12:25:32", "13/05/20 12:25:32"),
+                new Course(2, "testTitle2", "testType2", "13/05/20 12:25:32", "testInst2", "13/05/20 12:25:32", "13/05/20 12:25:32")
+        ).collect(Collectors.toList()));
         mockMvc.perform(get("/courses/")
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk()).andReturn();
+                .andExpect(status().isOk()).andExpect(content().json(globalConstants.ALL_COURSES));
+        verify(courseService).getAllCourses();
     }
 
     @Test
     public void testAddNewCourseSuccess() throws Exception {
-        String jsonBody = "{\n" +
-                "  \"title\": \"TestCourse\",\n" +
-                "  \"type\": \"testing\",\n" +
-                "  \"courseStartDate\": \"13/05/20 12:25:32\",\n" +
-                "  \"courseEndDate\": \"13/05/20 12:25:32\",\n" +
-                "  \"institutionId\": \"13/05/20 12:25:32\"\n" +
-                "}";
         MvcResult result = mockMvc.perform(post("/courses/")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonBody))
+                .content(globalConstants.COURSE_OBJECT))
                 .andExpect(status().isOk()).andReturn();
         String resultContent  = result.getResponse().getContentAsString();
         Response response = mapper.readValue(resultContent, Response.class);
@@ -77,13 +85,9 @@ public class CourseResourceTest {
     @Test
     public void testCourseEnrollmentSuccess() throws Exception {
         long courseId = 55;
-        String jsonBody = "{\n" +
-                "  \"wileyUserId\": 2,\n" +
-                "  \"userName\": \"testUser\"\n" +
-                "}";
         MvcResult result = mockMvc.perform(post("/courses/{courseId}/enrollments", courseId)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonBody))
+                .content(globalConstants.ENROLLMENT_OBJECT))
                 .andExpect(status().isOk()).andReturn();
         String resultContent  = result.getResponse().getContentAsString();
         Response response = mapper.readValue(resultContent, Response.class);
@@ -103,31 +107,22 @@ public class CourseResourceTest {
     @Test
     public void testUpdateExistingCourseSuccess() throws Exception {
         long courseId = 55;
-        String jsonObj = "{\n" +
-                "  \"wileyCourseId\": 55,\n" +
-                "  \"title\": \"Testing\",\n" +
-                "  \"type\": \"test\",\n" +
-                "  \"createDate\": \"14/05/20 16:28:48\",\n" +
-                "  \"institutionId\": \"inst2\",\n" +
-                "  \"courseStartDate\": \"13/05/20 12:25:32\",\n" +
-                "  \"courseEndDate\": \"13/05/20 12:25:32\"\n" +
-                "}";
         MvcResult result = mockMvc.perform(put("/courses/{courseId}", courseId)
                 .contentType(MediaType.APPLICATION_JSON)
-        .content(jsonObj)).andExpect(status().isOk()).andReturn();
+        .content(globalConstants.UPDATE_COURSE_OBJECT)).andExpect(status().isOk()).andDo(print()).andReturn();
     }
 
     @Test
     public void testGetAllEnrollmentsSuccess() throws Exception {
         long courseId = 55;
-        mockMvc.perform(get("/courses/55/enrollments")
+        mockMvc.perform(get("/courses/{courseId}/enrollments", courseId)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk()).andReturn();
     }
 
     @Test
     public void testSearchCourseByIdTestFail() throws Exception {
-        long courseId = 0;
+        String courseId = "0";
         mockMvc.perform(get("/courses/{courseId}", courseId)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound()).andReturn();
